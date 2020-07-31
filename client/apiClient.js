64bea744ad3dc0dbe8a4ace7fffdcc2e2c753e7a
Francisco Garcia <fgarcia.student04@gmail.com>
2020-07-31 02:35:52 -0400

    adding page forward and back

--- client/apiClient.js
+++ client/apiClient.js
@@ -1,53 +1,70 @@
-function apiClient() {
-  // search
-  let nextPage = 0;
-  let lastPage = 0;
-  let lastQuery = "";
+class apiClient {
+  constructor() {
+    // search
+    this.nextPage = 0;
+    this.lastPage = 0;
+    this.lastQuery = "";
 
-  // currently playing
-  let nextCurrentlyPlayingPage = 0;
-  let lastCurrentlyPlayingPage = 0;
-  return {
-    search: async (query, page = nextPage) => {
-      if (query !== lastQuery) {
-        nextPage = 0;
-        lastPage = 0;
-        lastQuery = query;
-        page = 0;
-      }
-      if (page !== 0 && page > lastPage) {
-        alert("No more movies available!");
-        return;
-      }
-      try {
-        const res = await fetch(`${client_env.apiUrl}/movies/search?query=${query}${page === 0 ? "" :`&page=${page}`}`)
-        const data = await res.json();
-        const currentPage = data.data.page;
-        lastPage = data.data.total_pages;
-        nextPage = currentPage + 1;
-        return data;
-      } catch (e) {
-        console.error(e);
-        alert("Could not fulfill request. Check console for more information.");
-      }
-    },
-    currentlyPlaying: async (page = nextCurrentlyPlayingPage) => {
-      if (page !== 0 && page > lastCurrentlyPlayingPage) {
-        alert("No more movies available!");
-        return;
-      }
-      try {
-        const res = await fetch(`${client_env.apiUrl}/movies/currently_playing${page === 0 ? "" :`?page=${page}`}`)
-        const data = await res.json();
-        const currentPage = data.data.page;
-        lastCurrentlyPlayingPage = data.data.total_pages;
-        nextCurrentlyPlayingPage = currentPage + 1;
-        return data;
-      } catch (e) {
-        console.error(e);
-        alert("Could not fulfill request. Check console for more information.");
-      }
-    } 
+    // currently playing
+    this.nextCurrentlyPlayingPage = 0;
+    this.lastCurrentlyPlayingPage = 0;
+  }
+
+  isEndSearch = (page = this.nextPage) => {
+    return page > this.lastPage;
+  }
+
+  isEndCurrentlyPlaying = (page = this.nextCurrentlyPlayingPage) => {
+    return page > this.lastCurrentlyPlayingPage;
+  }
+
+  search = async (query, page = this.nextPage) => {
+    this.nextCurrentlyPlayingPage = 0;
+    this.lastCurrentlyPlayingPage = 0;
+    if (query !== this.lastQuery) {
+      this.nextPage = 0;
+      this.lastPage = 0;
+      this.lastQuery = query;
+      page = 0;
+    }
+    if (page !== 0 && this.isEndSearch(page)) {
+      alert("No more movies available!");
+      return;
+    }
+    try {
+      const res = await fetch(`${client_env.apiUrl}/movies/search?query=${query}${page === 0 ? "" : `&page=${page}`}`);
+      const data = await res.json();
+      const currentPage = data.data.page;
+      this.lastPage = data.data.total_pages;
+      this.nextPage = currentPage + 1;
+      return data;
+    }
+    catch (e) {
+      console.error(e);
+      alert("Could not fulfill request. Check console for more information.");
+    }
+  }
+
+  currentlyPlaying = async (page = this.nextCurrentlyPlayingPage) => {
+    this.nextPage = 0;
+    this.lastPage = 0;
+    this.lastQuery = "";
+    if (page !== 0 && this.isEndCurrentlyPlaying(page)) {
+      alert("No more movies available!");
+      return;
+    }
+    try {
+      const res = await fetch(`${client_env.apiUrl}/movies/currently_playing${page === 0 ? "" : `?page=${page}`}`);
+      const data = await res.json();
+      const currentPage = data.data.page;
+      this.lastCurrentlyPlayingPage = data.data.total_pages;
+      this.nextCurrentlyPlayingPage = currentPage + 1;
+      return data;
+    }
+    catch (e) {
+      console.error(e);
+      alert("Could not fulfill request. Check console for more information.");
+    }
   }
 }
 
--- client/app.js
+++ client/app.js
@@ -1,4 +1,4 @@
-const api = apiClient();
+const api = new apiClient();
 const searchBar = document.getElementById("search");
 searchBar.addEventListener("keydown", async (e) => {
   const enterPressed = e.keyCode === 13;
@@ -9,6 +9,59 @@ searchBar.addEventListener("keydown", async (e) => {
   }
 });
 
+const mode = document.getElementById("mode");
+mode.addEventListener("click", async (e) => {
+  if (mode.value === "currentlyPlaying") {
+    mode.value = "search";
+    mode.innerText = "MODE: Search";
+    searchBar.style.display = "block";
+  } else {
+    mode.value = "currentlyPlaying";
+    mode.innerText = "MODE: Currently Playing";
+    searchBar.style.display = "none";
+    const data = await api.currentlyPlaying(0);
+    displayResults(data);
+  }
+
+})
+
+const prevPageBtn = document.getElementById("prev_page");
+prevPageBtn.addEventListener("click", async (e) => {
+  let data = {};
+  nextPageBtn.disabled = false;
+  if (mode.value === "currentlyPlaying") {
+    data = await api.currentlyPlaying(api.nextCurrentlyPlayingPage - 2);
+    if (api.nextCurrentlyPlayingPage - 2 <= 0) {
+      prevPageBtn.disabled = true;
+    }
+  } else {
+    data = await api.search(searchBar.value, api.nextPage - 2);
+    if (api.nextPage - 2 <= 0) {
+      prevPageBtn.disabled = true;
+    }
+  }
+  displayResults(data);
+});
+
+const nextPageBtn = document.getElementById("next_page");
+nextPageBtn.addEventListener("click", async (e) => {
+  let data = {};
+  prevPageBtn.disabled = false;
+  if (mode.value === "currentlyPlaying") {
+    data = await api.currentlyPlaying();
+    if (api.isEndCurrentlyPlaying()) {
+      nextPageBtn.disabled = true;
+    }
+  } else {
+    data = await api.search(searchBar.value);
+    if (api.isEndSearch()) {
+      nextPageBtn.disabled = true;
+    }
+  }
+
+  displayResults(data);
+});
+
 function removeAllChildNodes(parent) {
   while (parent.firstChild) {
     parent.removeChild(parent.firstChild);
@@ -16,10 +69,16 @@ function removeAllChildNodes(parent) {
 }
 
 function displayResults(data) {
+  if (!data) {
+    return;
+  }
   const resultsContainer = document.getElementById("results");
   removeAllChildNodes(resultsContainer);
   data.data.results.map((result) => {
     const movieCard = document.createElement("div");
+    movieCard.style.border = "1px solid black";
+    movieCard.style.padding = "5px";
+    movieCard.style.margin = "5px";
 
     const title = document.createElement("h2");
     title.innerText = result.title;
@@ -45,3 +104,6 @@ function displayResults(data) {
     resultsContainer.appendChild(movieCard);
   })
 }
+
+// initial data 
+api.currentlyPlaying().then(displayResults)
