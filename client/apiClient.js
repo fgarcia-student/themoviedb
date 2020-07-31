2a2ee80044939bce989ae92c9978eb67cdcd7c75
Francisco Garcia <fgarcia.student04@gmail.com>
2020-07-31 01:17:39 -0400

    adding currently playing movies function to client api

--- client/apiClient.js
+++ client/apiClient.js
@@ -1,7 +1,12 @@
 function apiClient() {
+  // search
   let nextPage = 0;
   let lastPage = 0;
   let lastQuery = "";
+
+  // currently playing
+  let nextCurrentlyPlayingPage = 0;
+  let lastCurrentlyPlayingPage = 0;
   return {
     search: async (query, page = nextPage) => {
       if (query !== lastQuery) {
@@ -20,12 +25,29 @@ function apiClient() {
         const currentPage = data.data.page;
         lastPage = data.data.total_pages;
         nextPage = currentPage + 1;
-        console.log({lastQuery, lastPage, nextPage})
-        console.log(data);
+        return data;
       } catch (e) {
         console.error(e);
         alert("Could not fulfill request. Check console for more information.");
       }
-    }
+    },
+    currentlyPlaying: async (page = nextCurrentlyPlayingPage) => {
+      if (page !== 0 && page > lastCurrentlyPlayingPage) {
+        alert("No more movies available!");
+        return;
+      }
+      try {
+        const res = await fetch(`${client_env.apiUrl}/movies/currently_playing${page === 0 ? "" :`?page=${page}`}`)
+        const data = await res.json();
+        const currentPage = data.data.page;
+        lastCurrentlyPlayingPage = data.data.total_pages;
+        nextCurrentlyPlayingPage = currentPage + 1;
+        return data;
+      } catch (e) {
+        console.error(e);
+        alert("Could not fulfill request. Check console for more information.");
+      }
+    } 
   }
 }
+
