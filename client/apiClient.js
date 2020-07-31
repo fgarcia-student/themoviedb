f387f2176a281cba7ed47e42039b0af5475357a8
Francisco Garcia <fgarcia.student04@gmail.com>
2020-07-31 01:09:16 -0400

    adding page override

--- client/apiClient.js
+++ client/apiClient.js
@@ -3,23 +3,24 @@ function apiClient() {
   let lastPage = 0;
   let lastQuery = "";
   return {
-    search: async (query) => {
+    search: async (query, page = nextPage) => {
       if (query !== lastQuery) {
         nextPage = 0;
         lastPage = 0;
         lastQuery = query;
+        page = 0;
       }
-      if (nextPage !== 0 && nextPage > lastPage) {
+      if (page !== 0 && page > lastPage) {
         alert("No more movies available!");
         return;
       }
       try {
-        console.log({lastQuery, lastPage, nextPage})
-        const res = await fetch(`${client_env.apiUrl}/movies/search?query=${query}${nextPage === 0 ? "" :`&page=${nextPage}`}`)
+        const res = await fetch(`${client_env.apiUrl}/movies/search?query=${query}${page === 0 ? "" :`&page=${page}`}`)
         const data = await res.json();
         const currentPage = data.data.page;
         lastPage = data.data.total_pages;
         nextPage = currentPage + 1;
+        console.log({lastQuery, lastPage, nextPage})
         console.log(data);
       } catch (e) {
         console.error(e);
